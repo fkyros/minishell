d20afa9c049a448e98b810b76c7970a243a9b8f5
fkyros <gade-oli@student.42madrid.com>
2025-04-06 19:30:32 +0200

    new env and SHLVL management for init copy of env

--- src/env/env.c
+++ src/env/env.c
@@ -6,37 +6,59 @@
 /*   By: gade-oli <gade-oli@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/27 16:00:26 by gade-oli          #+#    #+#             */
-/*   Updated: 2025/02/27 17:40:52 by gade-oli         ###   ########.fr       */
+/*   Updated: 2025/04/06 19:29:03 by event            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
 
-// NOTE: only expands `env` variables atm, not $? yet
-// char	**expand(char **matrix)
-// {
-// 	int	i;
-// 	char	*var_content;
-// 	char	*var_dup;
-	
-// 	i = 0;
-// 	while (matrix[i])
-// 	{
-// 		if (ft_strncmp("$", matrix[i], 1) == 0)
-// 		{
-// 			matrix[i]++;
-// 			var_content = getenv(matrix[i]);
-// 			var_dup = ft_strdup(var_content);
-// 			// free(matrix[i]);
-// 			matrix[i] = var_dup;
-// 		}
-// 		i++;
-// 	} 
-// 	return (matrix);
-// }
+//TODO: check negative numbers or invalid values for funny corrections
+char	*shlvl(char *lvl, char **new_env)
+{
+	int	nlvl;
+	char	**split;
+	char	*res;
+	char	*lvl_aux;
+
+	split = ft_split(lvl, '=');
+	if (!split || !split[2])
+		return (free_split(split), ft_strdup("SHLVL=0"));
+	nlvl = ft_atoi(split[2]);
+	lvl_aux = ft_itoa(++nlvl);
+	res = ft_strjoin("SHLVL=", lvl_aux);
+	free_split(split);
+	free(lvl_aux);
+	return (res);
+}
+
+char	**init_env(char **env)
+{
+	int	i;
+	char	**res;
+
+	i = 0;
+	while (env[i])
+		i++;
+	res = (char **) ft_calloc(i + 1, sizeof(char *));
+	if (!res)
+		return (NULL); //TODO: implement and execute safe exit
+	i = 0;
+	while (env[i])
+	{
+		if (ft_strncmp(res[i], "SHLVL=", 6)
+			res = shlvl(env[i], res);
+		else
+			res = ft_strdup(env[i]);
+		if (!res[i])
+			return (NULL); //TODO: implement and execute safe exit
+		i++;
+	}
+	return (res);
+}
 
 // TODO: implement this as a local get_env for new env
 // what if just $ ?
+// NOTE: only expands `env` variables atm, not $? yet
 char	*expand(char *var)
 {
 	char	*res;
