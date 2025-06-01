fa99cb7c6ef21377f0da6ca99f46d050fc85740d
fkyros <gade-oli@student.42madrid.com>
2025-06-01 13:40:09 +0200

    redirections.c norminette

--- src/redirs_pipes/redirections.c
+++ src/redirs_pipes/redirections.c
@@ -33,47 +33,62 @@ void	add_redirect(t_command *cmd, enum e_redirect_type type,
 	cmd->redir_count = new_count;
 }
 
+static int	handle_in_redir(t_redirect *r)
+{
+	int	fd;
+
+	fd = open(r->filename, O_RDONLY);
+	if (fd < 0)
+	{
+		perror(r->filename);
+		return (GENERIC_ERROR);
+	}
+	safe_dup2(fd, STDIN_FILENO);
+	close(fd);
+	return (0);
+}
+
+static int	handle_out_redir(t_redirect *r)
+{
+	int	fd;
+	int	flags;
+
+	flags = O_WRONLY | O_CREAT;
+	if (r->type == append)
+		flags |= O_APPEND;
+	else
+		flags |= O_TRUNC;
+	fd = open(r->filename, flags, 0644);
+	if (fd < 0)
+	{
+		perror(r->filename);
+		return (GENERIC_ERROR);
+	}
+	safe_dup2(fd, STDOUT_FILENO);
+	close(fd);
+	return (0);
+}
+
 int	apply_redirections(t_command *cmd)
 {
-	int			status;
-	int			fd;
 	int			i;
-	int			flags;
 	t_redirect	*r;
 
-	status = 0;
 	i = 0;
 	while (i < cmd->redir_count)
 	{
 		r = &cmd->redirs[i];
 		if (r->type == in && r->filename)
 		{
-			fd = open(r->filename, O_RDONLY);
-			if (fd < 0)
-			{
-				perror(r->filename);
-				return (1);
-			}
-			dup2(fd, STDIN_FILENO);
-			close(fd);
+			if (handle_in_redir(r) == GENERIC_ERROR)
+				return (GENERIC_ERROR);
 		}
 		else if ((r->type == out || r->type == append) && r->filename)
 		{
-			flags = O_WRONLY | O_CREAT;
-			if (r->type == append)
-				flags |= O_APPEND;
-			else
-				flags |= O_TRUNC;
-			fd = open(r->filename, flags, 0644);
-			if (fd < 0)
-			{
-				perror(r->filename);
-				return (1);
-			}
-			dup2(fd, STDOUT_FILENO);
-			close(fd);
+			if (handle_out_redir(r) == GENERIC_ERROR)
+				return (GENERIC_ERROR);
 		}
 		i++;
 	}
-	return (status);
+	return (0);
 }
